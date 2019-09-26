645975976823078fe0166cbf1203d7701989cd74
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-27 00:48:12 +0500

    First commit

---
+++ cQue/cQue/cNode.cpp
@@ -0,0 +1,35 @@
+//This program contains class named as cNode
+#include <iostream>
+using namespace std;
+//Declaring class cNode 
+class cNode {
+
+	//Private data member to hold value
+	int data;
+
+public:
+
+	//Declaring a pointer named as next of cNode data type
+	cNode *next;
+
+	//Default constructor	
+	cNode() {};
+
+	//Parameterized Constuctor
+	cNode(int d) : data(d) {};
+
+	//Function to get data
+	int getData() {
+		return data;
+	}
+
+	//Function to set Data
+	cNode & setData() {
+		this->data = data;
+	}
+
+	//Function to Print Data
+	void print() {
+		cout << data << " ";
+	}
+};
