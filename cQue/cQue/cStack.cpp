2ebe88d78560ffe38ab640cb9ed08184e6d6f531
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-27 00:46:21 +0500

    First commit

---
+++ cQue/cQue/cStack.cpp
@@ -0,0 +1,139 @@
+//This program contains cStack class 
+
+#include <iostream>
+#include "cNode.cpp"
+
+using namespace std;
+
+//Declared Cstack class
+class cStack {
+
+	cNode *top;
+
+public:
+
+	cStack() :top(NULL) {};
+	cStack(cNode *& ptr) : top(ptr) {
+		top->next = NULL;
+		ptr = NULL;
+	}
+
+	bool isEmpty() const{
+		if (top == NULL) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool isNotEmpty() const {
+		if (top != NULL) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	cStack & push(cNode *& ptr) {
+		ptr->next = top;
+		top = ptr;
+		ptr = NULL;
+		return * this;
+	}
+
+	cNode * pop() {
+		cNode * ptr = top;
+		top = top->next;
+		ptr->next = NULL;
+	}
+
+	void print() const {
+		if (!top) {
+			cout << "/nIt is empty/n";
+		}
+		else {
+			cout << "/nThe data is:/n";
+			cNode *ptr = top;
+			while (ptr) {
+				ptr->print();
+				ptr = ptr->next;
+			}
+		}
+	}
+
+	cStack(const cStack & src) {
+		this->top = src.top;
+		if (src.top) {
+			cNode *sptr, *dptr;
+			dptr = top = new cNode(*src.top);
+			sptr = src.top->next;
+
+			while (sptr) {
+				dptr->next = new cNode(*sptr);
+				sptr = sptr->next;
+				dptr = dptr->next;
+			}
+		}
+	}
+
+	cStack & operator = (const cStack & obj) {
+		if (this == &obj) {
+			return *this;
+		}
+		if (true) {
+			cStack temp;
+			temp.top = top;
+		}
+		if (true) {
+			cStack temp;
+			temp = obj;
+			top = temp.top;
+			temp.top = NULL;
+			return *this;
+		}
+	}
+
+	~cStack() {
+		cNode *ptr = top;
+		while (ptr) {
+			ptr = ptr->next;
+			delete top;
+			top = ptr;
+		}
+	}
+
+};
+
+class cQue : protected cStack {
+	cNode *tail;
+public:
+	cQue() :tail(NULL) {};
+	cQue(cNode *& ptr) :cStack(ptr) { tail(top);}
+
+	bool isNotEmpty() const {
+		return cStack::isEmpty();
+	}
+
+	cNode * remove() {
+		if (!top->next) {
+			tail = NULL;
+		}
+		return cStack::pop();
+	}
+
+	cQue(const cQue & src) :cStack((cStack)&src) {
+
+		tail = top;
+		while (tail) 
+			if (tail == NULL) {
+				break;
+			}
+			tail = tail->next;
+		}
+
+	}
+
+
+};
