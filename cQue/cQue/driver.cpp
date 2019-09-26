39752b0a5cebbbe09859d3cb0621ece7d2f28ff6
Ghulam Ghous <itsghulamghous@gmail.com>
2019-09-27 00:48:37 +0500

    First commit

---
+++ cQue/cQue/driver.cpp
@@ -0,0 +1,14 @@
+
+/*
+The driver program to check everything defined in other heads
+*/
+
+#include<iostream>
+#include"cNode.cpp"
+#include"cStack.cpp"
+
+using namespace std;
+
+int main() {
+	return 0;
+}
