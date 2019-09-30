52b80881f65d7080b77604396fdbd9db98d9dd29
Ghulam Ghous <itsghulamghous@gmail.com>
2019-10-01 02:16:37 +0500

    Added code to test cStack class

--- Stack/Stack/main.cpp
+++ Stack/Stack/main.cpp
@@ -5,8 +5,19 @@
 using namespace std;
 
 int main() {
+
+	//Declaring stack objects
 	cStack obj1;
-	cNode obj2(20);
-	obj1.push(&obj2);
+	cNode *ptr = new cNode(3823);
+	obj1.push(ptr);
+	cNode *ptr1 = new cNode(38123);
+	obj1.push(ptr1);
+	cNode *ptr2 = new cNode(381123);
+	obj1.push(ptr2);
+	cout << "Printing the stack" << endl;
 	obj1.print();
+	obj1.pop();
+	obj1.print();
+	system("pause");
+	return 0;
 }
