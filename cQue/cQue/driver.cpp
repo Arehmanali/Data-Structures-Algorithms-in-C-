c984c3ebf219d263b2143d7a37c0823bda88122c
Ghulam Ghous <itsghulamghous@gmail.com>
2019-10-01 02:04:03 +0500

    Added code to check cQue class and cStack class

--- cQue/cQue/driver.cpp
+++ cQue/cQue/driver.cpp
@@ -9,47 +9,36 @@ The driver program to check everything defined in other heads
 
 using namespace std;
 
-int operation(char opr, int *index);
 
 int main() {
 	
 	//Declaring stack objects
 	cStack obj1;
-	cNode *n[5];
-	n[0] = new cNode(1);
-	n[1] = new cNode(2);
-	n[2] = new cNode(3);
-	n[3] = new cNode('/');
-	n[4] = new cNode('-');
-	n[5] = new cNode(4);
-	n[6] = new cNode(5);
-	n[7] = new cNode('+');
-	n[8] = new cNode('*');
-
-	obj1.push(n[0]);
-	obj1.push(n[1]);
-	obj1.push(n[2]);
-	obj1.push(n[3]);
-	obj1.push(n[4]);
-	obj1.push(n[5]);
-	obj1.push(n[6]);
-	obj1.push(n[7]);
-	obj1.push(n[8]);
+	cNode *ptr = new cNode(3823);
+	obj1.push(ptr);
+	cNode *ptr1 = new cNode(38123);
+	obj1.push(ptr1);
+	cNode *ptr2 = new cNode(381123);
+	obj1.push(ptr2);
 	cout << "Printing the stack" << endl;
 	obj1.print();
+	obj1.pop();
+	obj1.print();
+	
 
-	return 0;
-}
 
-int operation(char opr, int *index) {
-	if (opr == '+')
-		return *index + *(index + 1);
-	else if (opr == '-')
-		return *index - *(index + 1);
-	else if (opr == '*')
-		return *index * *(index + 1);
-	else if (opr == '/')
-		return *index / *(index + 1);
-	else
-		return *index % *(index + 1);
+
+	cque obj2;
+	cNode *ptr3 = new cNode(44444);
+	obj2.add(ptr3);
+	cNode *ptr4 = new cNode(44445);
+	obj2.add(ptr4);
+	cNode *ptr5 = new cNode(44446);
+	obj2.add(ptr5);
+	cNode *ptr6 = new cNode(44447);
+	obj2.add(ptr6);
+	obj2.print();
+	obj2.remove();
+	obj2.print();
+	system("pause");
 }
