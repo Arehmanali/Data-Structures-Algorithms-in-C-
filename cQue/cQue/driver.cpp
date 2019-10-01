11987c256976fd428689dd6d003888a8e4001fc1
Ghulam Ghous <itsghulamghous@gmail.com>
2019-10-01 18:57:13 +0500

    Checked copy constructor and assignment operator

--- cQue/cQue/driver.cpp
+++ cQue/cQue/driver.cpp
@@ -12,35 +12,29 @@ using namespace std;
 
 int main() {
 	
-	//Declaring stack objects
-	cStack obj1;
-	cNode *ptr = new cNode(3823);
-	obj1.push(ptr);
-	cNode *ptr1 = new cNode(38123);
-	obj1.push(ptr1);
-	cNode *ptr2 = new cNode(381123);
-	obj1.push(ptr2);
-	cout << "Printing the stack" << endl;
-	obj1.print();
-	obj1.pop();
-	obj1.print();
-	
+	cque que1;
 
+	cNode *ptr3 = new cNode(1000);
+	que1.add(ptr3);
 
+	cNode *ptr4 = new cNode(2000);
+	que1.add(ptr4);
 
-	cque obj2;
-	cNode *ptr3 = new cNode(44444);
-	obj2.add(ptr3);
-	cNode *ptr4 = new cNode(44445);
-	obj2.add(ptr4);
-	cNode *ptr5 = new cNode(44446);
-	obj2.add(ptr5);
-	cNode *ptr6 = new cNode(44447);
-	obj2.add(ptr6);
-	obj2.print();
-	cque obj3(obj2);
-	obj3.print();
-	cque obj4 = obj3;
-	obj4.print();
+	cNode *ptr5 = new cNode(3000);
+	que1.add(ptr5);
+
+	cNode *ptr6 = new cNode(4000);
+	que1.add(ptr6);
+
+	que1.print();
+
+	//Testing copy constructor
+	cque que2(que1);
+	que2.print();
+
+	//Testing Overloaded assignment operator
+	cque que3 = que2;
+	que3.print();
+
 	system("pause");
 }
